247e45024f382b844a807e418c50e200761598e1
tt1989182-cyber <tt1989182@gmail.com>
2025-12-17 22:56:06 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,8 +1,7 @@
 
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
