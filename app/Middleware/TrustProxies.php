ecf573640402c73fbf95aaca7bed925655227f4a
tt1989182-cyber <tt1989182@gmail.com>
2025-12-18 00:00:54 +0700

    Create TrustProxies.php

---
+++ app/Middleware/TrustProxies.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Symfony\Component\HttpFoundation\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * Trust all proxies (Render)
+     */
+    protected $proxies = '*';
+
+    /**
+     * Trust X-Forwarded-* headers
+     */
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO;
+}
