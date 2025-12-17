1f8f4cc36d7632891ae9a1411597fbea757f2ec1
tt1989182-cyber <tt1989182@gmail.com>
2025-12-18 00:39:44 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,16 +3,23 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
     public function register(): void
     {
         //
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
-      URL::forceScheme('https');
+        URL::forceScheme('https');
     }
 }
