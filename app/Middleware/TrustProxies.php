d2ecb42a2c98a35abaa1361c724e2f9a2c4e9f8b
tt1989182-cyber <tt1989182@gmail.com>
2025-12-18 00:20:58 +0700

    Update TrustProxies.php

--- app/Middleware/TrustProxies.php
+++ app/Middleware/TrustProxies.php
@@ -2,19 +2,13 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Http\Middleware\TrustProxies as Middleware;
-use Symfony\Component\HttpFoundation\Request;
+use Illuminate\Http\Request;
+use Fideloper\Proxy\TrustProxies as Middleware;
 
 class TrustProxies extends Middleware
 {
-    /**
-     * Trust all proxies (Render)
-     */
     protected $proxies = '*';
 
-    /**
-     * Trust X-Forwarded-* headers
-     */
     protected $headers =
         Request::HEADER_X_FORWARDED_FOR |
         Request::HEADER_X_FORWARDED_HOST |
